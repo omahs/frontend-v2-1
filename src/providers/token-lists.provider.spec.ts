4c8dfb200f3bc980a3e2ab821b54e9f5536dceb7
sssmi <simeon.kerkola@gmail.com>
2023-01-10 19:12:44 +0200

    Fix tests

--- src/providers/token-lists.provider.spec.ts
+++ src/providers/token-lists.provider.spec.ts
@@ -21,9 +21,22 @@ jest.mock('@/services/web3/useWeb3');
 describe('Token lists provider should', () => {
   test('provide active TokenList', async () => {
     const { result } = mountComposableWithTokenLists();
-    expect(firstTokenListSymbols(result.activeTokenLists.value)).toEqual(
-      '["BAL","DAI","USDT","USDC","WETH","WBTC","miMATIC","bb-a-usd2","bb-a-USDT","bb-a-USDC","bb-a-DAI","USDC","DAI","USDT"]'
-    );
+    expect(firstTokenListSymbols(result.activeTokenLists.value)).toEqual([
+      'BAL',
+      'DAI',
+      'USDT',
+      'USDC',
+      'WETH',
+      'WBTC',
+      'miMATIC',
+      'bb-a-usd2',
+      'bb-a-USDT',
+      'bb-a-USDC',
+      'bb-a-DAI',
+      'USDC',
+      'DAI',
+      'USDT',
+    ]);
   });
 
   test('provide approved TokenList', async () => {
